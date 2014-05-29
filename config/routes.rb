0b824bc00b78e5e41012b2c8494c60b6992b585a
Trevor <pleadnocontest@gmail.com>
2014-05-29 15:33:51 -0700

    added tagging functionality, no style yet

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Blogger::Application.routes.draw do
   
+  root to: 'articles#index'
+  
   resources :articles do
     resources :comments
   end
   
-  root to: 'articles#index'
+  resources :tags
+  
+  
 end
